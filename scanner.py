d078bfb8925521b40b73fc16e6e58ac5c301e058
Akami-Slayer <akamislayer553@gmail.com>
2020-11-23 11:45:32 -0500

    Port Scanner - Beta - full ports

--- scanner.py
+++ scanner.py
@@ -19,10 +19,11 @@ print ("Time started: " + str(datetime.now()))
 print ('-' * 50)
 
 try:
-	for port in range(50,85):
+	for port in range(1,65535):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket.setdefaulttimeout(1)
 		result = s.connect_ex((target,port)) #returns an error indicator
+		#print ("Checking port {}".format(port))
 		if result == 0:
 			print ("Port {} is open".format(port))
 		s.close()
